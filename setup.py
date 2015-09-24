195625bcaa67d157e70ac8a454a3f8674ed29e8a
patrick <plee@prh.com>
2015-09-23 22:02:45 -0400

    prepping for pypi package upload

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
 setup(name='Geckopush',
-      version='0.2.0-dev',
-      description="A library for geckoboard widgets' push functionality",
+      packages=['Geckopush'],
+      version='0.1',
+      description="A library for geckoboard push functionality",
       author="Patrick Lee",
       author_email="me@patricklee.nyc",
       url="http://www.github.com/patleeman/geckopush",
-      packages=['Geckopush'],
+      download_url="http://www.github.com/patleeman/geckopush/tarball/0.1",
+      keywords = ['geckoboard', 'api', 'widget'],
+      classifiers=[]
       )
